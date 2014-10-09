61ff9bb2aeed021c011bc2bcf062009f979c0edf
Thomas Lackemann <tommylackemann@gmail.com>
2014-10-09 00:37:02 -0400

    added grunt tasks and a traffic.game.min.js file (attempting to keep under 36kb total)

--- public/js/traffic.game.js
+++ public/js/traffic.game.js
@@ -1,5 +1,5 @@
 /**
- * Script file for handling basic UX
+ * Traffic Game
  *
  * @package		Traffic
  * @version		1.0.0
@@ -45,6 +45,13 @@
 		var image = new Image();
 
 		/**
+		 * The method to run when render() is called
+		 * @protected
+		 * @var {Function}
+		 */
+		var renderMethod = function() {};
+
+		/**
 		 * X position
 		 * @var {Number}
 		 */
@@ -63,11 +70,21 @@
 		this.speed = options.speed || 0,
 
 		/**
-		 * Draw the entity
+		 * Set the render method
+		 * @param {Function} method
 		 * @return {Entity}
 		 */
-		this.draw = function() {
+		this.onRender = function(method) {
+			renderMethod = method;
 			return this;
+		},
+
+		/**
+		 * Render the Entity based on the renderMethod
+		 * @return void
+		 */
+		this.render = function() {
+			renderMethod();
 		},
 
 		/**
@@ -164,7 +181,48 @@
 		 * CTX object
 		 * @var {Object}
 		 */
-		this.ctx = this.canvas.getContext('2d'),
+		this.ctx = this.canvas.getContext('2d');
+
+		/**
+		 * Check if keys are pressed during update()
+		 * @protected
+		 * @param {Number} modifier
+		 * @return void
+		 */
+		var _checkKeys = function(modifier) {
+			if (38 in keysDown) { // Player holding up
+				self.entity('hero').y -= self.entity('hero').speed * modifier;
+			}
+			if (40 in keysDown) { // Player holding down
+				self.entity('hero').y += self.entity('hero').speed * modifier;
+			}
+			if (37 in keysDown) { // Player holding left
+				self.entity('hero').x -= self.entity('hero').speed * modifier;
+			}
+			if (39 in keysDown) { // Player holding right
+				self.entity('hero').x += self.entity('hero').speed * modifier;
+			}
+		};
+
+
+		/**
+		 * Checks for entity collision during update()
+		 * @protected
+		 * @param {Number} modifier
+		 * @return void
+		 */
+		var _checkCollision = function(modifier) {
+			// Are they touching?
+			if (
+				self.entity('hero').x <= (self.entity('monster').x + 32)
+				&& self.entity('monster').x <= (self.entity('hero').x + 32)
+				&& self.entity('hero').y <= (self.entity('monster').y + 32)
+				&& self.entity('monster').y <= (self.entity('hero').y + 32)
+			) {
+				//++monstersCaught;
+				self.reset();
+			}
+		};
 
 		/**
 		 * Sets and starts a new Entity
@@ -201,14 +259,37 @@
 			this.setEntity('background', {
 				imageSrc: 'images/background.png'
 			});
+			// Set the background images draw method
+			this.entity('background').onRender(function() {
+				var background = self.entity('background');
+				if (background.isReady()) {
+					self.ctx.drawImage(self.entity('background').getImage(), 0, 0);
+				}
+			});
+
 			// Hero image
 			this.setEntity('hero', {
 				imageSrc: 'images/hero.png',
 				speed: 256
 			});
+			// Set the heros images draw method
+			this.entity('hero').onRender(function() {
+				var hero = self.entity('hero');
+				if (hero.isReady()) {
+					self.ctx.drawImage(hero.getImage(), hero.x, hero.y);
+				}
+			});
+
 			// Monster image
 			this.setEntity('monster', {
 				imageSrc: 'images/monster.png'
+			});
+			// Set the monster images draw method
+			this.entity('monster').onRender(function() {
+				var monster = self.entity('monster');
+				if (monster.isReady()) {
+					self.ctx.drawImage(monster.getImage(), monster.x, monster.y);
+				}
 			});
 
 			// Add event listeners
@@ -242,29 +323,8 @@
 		 * @return void
 		 */
 		this.update = function(modifier) {
-			if (38 in keysDown) { // Player holding up
-				this.entity('hero').y -= this.entity('hero').speed * modifier;
-			}
-			if (40 in keysDown) { // Player holding down
-				this.entity('hero').y += this.entity('hero').speed * modifier;
-			}
-			if (37 in keysDown) { // Player holding left
-				this.entity('hero').x -= this.entity('hero').speed * modifier;
-			}
-			if (39 in keysDown) { // Player holding right
-				this.entity('hero').x += this.entity('hero').speed * modifier;
-			}
-
-			// Are they touching?
-			if (
-				this.entity('hero').x <= (this.entity('monster').x + 32)
-				&& this.entity('monster').x <= (this.entity('hero').x + 32)
-				&& this.entity('hero').y <= (this.entity('monster').y + 32)
-				&& this.entity('monster').y <= (this.entity('hero').y + 32)
-			) {
-				//++monstersCaught;
-				this.reset();
-			}
+			_checkKeys(modifier);
+			_checkCollision(modifier);
 		},
 
 		/**
@@ -272,21 +332,9 @@
 		 * @return void
 		 */
 		this.render = function() {
-			var hero = this.entity('hero'),
-				monster = this.entity('monster'),
-				background = this.entity('background');
-
-			if (background.isReady()) {
-				this.ctx.drawImage(background.getImage(), 0, 0);
-			}
-
-			if (hero.isReady()) {
-				this.ctx.drawImage(hero.getImage(), hero.x, hero.y);
-			}
-
-			if (monster.isReady()) {
-				this.ctx.drawImage(monster.getImage(), monster.x, monster.y);
-			}
+			this.entity('background').render();
+			this.entity('hero').render();
+			this.entity('monster').render();
 
 			// Score
 			this.ctx.fillStyle = "rgb(250, 250, 250)";
@@ -303,12 +351,9 @@
 		this.main = function() {
 			now = Date.now();
 			delta = now - then;
-
 			self.update(delta / 1000);
 			self.render();
-
 			then = now;
-
 			// Request to do this again ASAP
 			requestAnimationFrame(self.main);
 		};
